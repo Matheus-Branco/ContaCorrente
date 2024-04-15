760266bad62bffe5ec10fd3333b8f76f2a293ab2
Matheus-Branco <112667064+Matheus-Branco@users.noreply.github.com>
2024-04-15 11:08:44 -0300

    Update Movimentação.cs

--- Movimentação.cs
+++ Movimentação.cs
@@ -1,54 +1,17 @@
-﻿using System;
-
 public class Movimentacao
-{
-	public Movimentacao()
-	{
-        public void ViewSaldo() 
+    {
+        public double Valor { get; private set; }
+        public TipoMovimentacao Tipo { get; private set; }
+
+        public Movimentacao(double valor, TipoMovimentacao tipo)
         {
-            Cliente Matheus = new Cliente("Matheus ", "Jeremias", 012, 10000, false, 1000000, "012.257.839.25");
-
-            Console.WriteLine(Matheus.saldo);
-
-        }
-        public void Sacar()
-        {
-            Cliente Matheus = new Cliente("Matheus ", "Jeremias", 012, 10000, false, 1000000, "012.257.839.25");
-            Cliente Fulano = new Cliente("Fulano", "De tal", 012, 10000, false, 1000000, "012.257.839.25");
-
-            decimal QuantSaquar = 0;
-            Console.WriteLine($"Seu saldo é {Sacar.saldo}. Quando você deseja saquar?");
-
-            QuantSaquar = 100;
-
-            if (QuantSaquar > limite + saldo)
-            {
-                Console.WriteLine($"Você não pode saquar");
-            }
-            else
-            {
-                saldo = QuantSaquar - saldo;
-                Console.WriteLine($"Seu saldo atual é {saldo}");
-            }
-        }
-        public void Depositar()
-        {
-            Cliente Depositar = new Cliente("Matheus Jeremias", 012, 10000, false, 1000000, "012.257.839.25");
-
-            decimal QuantDepo = 0;
-            Console.WriteLine($"Seu saldo é {Sacar.saldo}. Quando você deseja saquar?");
-
-            QuantDepo = 100;
-            
-                saldo = QuantDepo + saldo;
-                Console.WriteLine($"Seu saldo atual é {saldo}");
-        }
-        public void Transferir()
-        {
-            Cliente Matheus = new Cliente("Matheus ", "Jeremias", 012, 10000, false, 1000000, "012.257.839.25");
-            Cliente Fulano = new Cliente("Fulano", "De tal", 012, 10000, false, 1000000, "012.257.839.25");
-
-            Matheus.saldo = Fulano.saldo + Matheus.saldo;
+            Valor = valor;
+            Tipo = tipo;
         }
     }
-}
+
+    public enum TipoMovimentacao
+    {
+        Credito,
+        Debito
+    }
